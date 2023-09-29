b0a1282365ff87c5173b000bdb1eab80f09b7102
ramakrishnat19 <dakshsr.t19@gmail.com>
2023-09-30 02:11:10 +0530

    initial layout

---
+++ ePortal.UI/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModule } from './login/login.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LoginModule,
+    ReactiveFormsModule,
+    FormsModule,
+     
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
